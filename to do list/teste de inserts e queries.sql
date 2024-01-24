a32790a3f772fb9606205cc9278f0151f6d7f4e9
Gustavo Marques <91503740+MarquesGusta@users.noreply.github.com>
2024-01-24 11:04:19 -0300

    Add files via upload

---
+++ to do list/teste de inserts e queries.sql
@@ -0,0 +1,32 @@
+insert into listas (nomeLista) values ("Mercado");
+insert into listas (nomeLista) values ("Materiais escolares");
+insert into listas (nomeLista) values ("Tarefas");
+select * from listas;
+
+-- SET @data_formatada = DATE_FORMAT(@data_formatada, '%d/%m/%Y');
+-- select @data_formatada;
+
+truncate tarefas;
+select * from tarefas;
+insert into tarefas (fk_idLista, nomeTarefa, descricao, dataCriacao, dataEntrega, status) values (1, "Arroz", null , curdate(), null, "feito");
+insert into tarefas (fk_idLista, nomeTarefa, descricao, dataCriacao, dataEntrega, status) values (1, "Feijão", null , curdate(), null, "nao feito");
+insert into tarefas (fk_idLista, nomeTarefa, descricao, dataCriacao, dataEntrega, status) values (1, "Carne", null , curdate(), null, "feito");
+insert into tarefas (fk_idLista, nomeTarefa, descricao, dataCriacao, dataEntrega, status) values (1, "Batata", null , curdate(), null, "nao feito");
+insert into tarefas (fk_idLista, nomeTarefa, descricao, dataCriacao, dataEntrega, status) values (2, "Grafite de lapizeira", "2.0", curdate(), null, "nao feito");
+insert into tarefas (fk_idLista, nomeTarefa, descricao, dataCriacao, dataEntrega, status) values (2, "Caderno do batman", "10 matérias", curdate(), null, "nao feito");
+insert into tarefas (fk_idLista, nomeTarefa, descricao, dataCriacao, dataEntrega, status) values (2, "Borracha", null , curdate(), null, "feito");
+insert into tarefas (fk_idLista, nomeTarefa, descricao, dataCriacao, dataEntrega, status) values (2, "Apontador", null, curdate(), null, "feito");
+insert into tarefas (fk_idLista, nomeTarefa, descricao, dataCriacao, dataEntrega, status) values (3, "Cortar a grama", "Apenas a parte de trás do quintal", curdate(), "2024/01/23", "feito");
+insert into tarefas (fk_idLista, nomeTarefa, descricao, dataCriacao, dataEntrega, status) values (3, "Fazer a lição de casa", null, curdate(), "2024-02-10", "nao feito");
+insert into tarefas (fk_idLista, nomeTarefa, descricao, dataCriacao, dataEntrega, status) values (3, "Varrer a casa", "só o andar de baixo", curdate(), "2024-01-25", "nao feito");
+
+select
+	*
+from
+	listas
+join
+	tarefas
+on
+	pk_idLista = fk_idlista
+where
+	nomeLista = "Mercado" AND status = "nao feito"
---
+++ to do list/todolist_script.sql
@@ -0,0 +1,23 @@
+create database toDoList;
+
+use toDoList;
+
+drop database toDoList;
+
+create table listas(
+	pk_idLista int PRIMARY KEY AUTO_INCREMENT,
+    nomeLista varchar(30) NOT NULL
+);
+
+create table tarefas(
+	pk_idTarefa int PRIMARY KEY AUTO_INCREMENT,
+    fk_idLista int NOT NULL,
+    nomeTarefa varchar(30) NOT NULL,
+    descricao varchar(200),
+    dataCriacao date NOT NULL,
+    dataEntrega date,
+    status enum("feito", "nao feito") NOT NULL,
+    
+    -- CHECK(dataEntrega ("--/--/----")),
+    FOREIGN KEY(fk_idLista) REFERENCES listas(pk_idLista)
+);
